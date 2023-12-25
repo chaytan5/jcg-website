a579a2d6f4b301b4f72e0e52bce9a836a9965570
Chetan Arora <chaytanarora@gmail.com>
2023-12-26 00:05:51 +0530

    env file validateion

--- dbConfig/dbConfig.ts
+++ dbConfig/dbConfig.ts
@@ -2,19 +2,23 @@ import mongoose from "mongoose";
 
 export async function connect() {
   try {
-    mongoose.connect(process.env.MONGO_URI!);
-    const connection = mongoose.connection;
+    if (process.env.MONGO_URI) {
+      mongoose.connect(process.env.MONGO_URI);
+      const connection = mongoose.connection;
 
-    connection.on("connected", () => {
-      console.log("MongoDB connected succesfully");
-    });
+      connection.on("connected", () => {
+        console.log("MongoDB connected succesfully");
+      });
 
-    connection.on("error", (error) => {
-      console.log(
-        "MongoDB connection error. Make sure MongoDB is running " + error,
-      );
-      process.exit();
-    });
+      connection.on("error", (error) => {
+        console.log(
+          "MongoDB connection error. Make sure MongoDB is running " + error,
+        );
+        process.exit();
+      });
+    } else {
+      console.log("ENV file not loaded correctly");
+    }
   } catch (error) {
     console.log("Something went wrong");
     console.log(error);
